a47fef70af73ce4a3db78e403d8eca50e3c330fe
Lord King <kingoflordscz@gmail.com>
2025-05-21 17:04:52 +0200

    Delete BlockBreakEvent.java

--- src/main/java/com/michalovec/eventhelper/Listeners/BlockBreakEvent.java
+++
@@ -1,9 +0,0 @@
-package com.michalovec.eventhelper.Listeners;
-
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
-public class BlockBreakEvent implements Listener {
-
-    //
-}
