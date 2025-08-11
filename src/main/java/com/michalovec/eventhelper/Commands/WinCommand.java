ca0a58af77a5f1d102d7880cf4c90a73eef629c9
smutnejmichal <michal.smutny08@gmail.com>
2025-08-11 12:50:09 +0200

    Design

--- src/main/java/com/michalovec/eventhelper/Commands/WinCommand.java
+++ src/main/java/com/michalovec/eventhelper/Commands/WinCommand.java
@@ -24,51 +24,51 @@ public class WinCommand implements CommandExecutor {
 
         StatsManager statsManager = plugin.getStatsManager();
 
+        final String prefix = "§x§E§6§F§1§1§B§lW§x§E§9§F§2§3§D§li§x§E§C§F§4§5§F§ln§x§E§F§F§5§8§1§ls §8• §7";
+
         if (args.length == 0) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.RED + "Pouze hráči mohou vidět své výhry.");
+                sender.sendMessage(prefix + "§cPouze hráči mohou vidět své výhry.");
                 return true;
             }
 
             Player player = (Player) sender;
             int wins = statsManager.getWins(player.getName());
-            player.sendMessage(ChatColor.GREEN + "Tvoje výhry: " + ChatColor.GOLD + wins);
+            player.sendMessage(prefix + "Tvoje výhry: §x§E§6§F§1§1§B" + wins);
             return true;
         }
 
         if (args.length == 2 && args[0].equalsIgnoreCase("add")) {
             if (!sender.hasPermission("eventhelper.addwins")) {
-                sender.sendMessage(ChatColor.RED + "Nemáš oprávnění přidávat výhry.");
+                sender.sendMessage(prefix + "§cNemáš oprávnění přidávat výhry.");
                 return true;
             }
 
             Player target = Bukkit.getPlayer(args[1]);
             if (target == null) {
-                sender.sendMessage(ChatColor.RED + "Hráč '" + args[1] + "' není online.");
+                sender.sendMessage(prefix + "Hráč §x§E§6§F§1§1§B" + args[1] + " §7není online.");
                 return true;
             }
 
             statsManager.addWin(target.getName());
-            sender.sendMessage(ChatColor.GREEN + "Přidána výhra hráči " + ChatColor.YELLOW + target.getName());
-            target.sendMessage(ChatColor.GOLD + "Získal jsi novou výhru!");
+            sender.sendMessage(prefix + "Přidána výhra hráči §x§E§6§F§1§1§B" + target.getName() + "§7.");
+            target.sendMessage(prefix + "Získal jsi novou výhru!");
 
             return true;
         }
 
         if (args.length == 1 && args[0].equalsIgnoreCase("clear")) {
             if (!sender.hasPermission("eventwins.clear")) {
-                sender.sendMessage(ChatColor.RED + "Nemáš oprávnění smazat všechny výhry.");
+                sender.sendMessage(prefix + "§cNemáš oprávnění smazat všechny výhry.");
                 return true;
             }
 
             statsManager.clearAll();
-            sender.sendMessage(ChatColor.GREEN + "Všechny výhry byly úspěšně smazány.");
+            sender.sendMessage(prefix + "Všechny výhry byly úspěšně smazány.");
             return true;
         }
 
-        sender.sendMessage(ChatColor.RED + "Použití:");
-        sender.sendMessage(ChatColor.YELLOW + "/" + label + ChatColor.GRAY + " - zobrazí tvoje výhry");
-        sender.sendMessage(ChatColor.YELLOW + "/" + label + " add <hráč>" + ChatColor.GRAY + " - přidá výhru hráči");
+        sender.sendMessage(prefix + "§cŠpatné použití. Použij: /win");
         return true;
     }
 }
